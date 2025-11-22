76c348ba480478a3285609ef32f360135f75cd46
lavanyadhanasekar <lavanyadhana398@gmail.com>
2025-11-22 23:19:11 +0530

    Add files via upload

---
+++ lab3.v
@@ -0,0 +1,12 @@
+module lab3(
+    input  wire a, b,     // Inputs
+    output wire sum,      // Sum output
+    output wire carry     // Carry output
+);
+
+    // Logic equations
+    assign sum   = a ^ b;   // XOR for sum
+    assign carry = a & b;   // AND for carry
+
+endmodule
+
